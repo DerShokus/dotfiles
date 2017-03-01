d2a58eaeaef2e25a22e5fac6ccb4e65aa36a7a97
DerShokus <lily.coder@gmail.com>
2017-03-01 22:52:45 +0300

    settings for emacs

--- init.el
+++ init.el
@@ -5,6 +5,9 @@
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize)
 
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
 (global-set-key [f5] (lambda () (interactive)
 		       (load-file "~/.emacs.d/init.el")))
 
@@ -12,16 +15,31 @@
 
 (load-theme 'whiteboard)
 
+(scroll-bar-mode -1)
+
+(setq make-backup-files nil) ; ~files
+(setq auto-save-default nil) ; #files#
+
+(use-package markdown-mode
+	     :ensure t
+	     :commands (markdown-mode gfm-mode)
+	     :mode (("README\\.md\\'" . gfm-mode)
+		    ("\\.md\\'" . markdown-mode)
+		    ("\\.markdown\\'" . markdown-mode))
+	     :init (setq markdown-command "multimarkdown"))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (helm-themes helm))))
+ '(package-selected-packages
+   (quote
+    (exec-path-from-shell markdown-mode helm-themes helm))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
